e418d705c44d03cd19c3cf215f1e4b153fcf3965
ToanMobile <toan.huynh@bstarsolutions.com>
2024-09-16 13:35:53 +0700

    update flutter 3.24

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=D:\Software\SDK\flutter"
-export "FLUTTER_APPLICATION_PATH=F:\WorkSet\Other\get_version\example"
+export "FLUTTER_ROOT=/Users/toan.huynh/fvm/versions/3.7.11"
+export "FLUTTER_APPLICATION_PATH=/Users/toan.huynh/Documents/FinOS/get_version/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
